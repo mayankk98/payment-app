5fef3add32d9694328166d832bb7f38020ffa446
root <mayankkhandelwal92@gmail.com>
2018-10-18 00:53:18 +0530

    payment page complete

--- index.php
+++ index.php
@@ -9,21 +9,26 @@
 	<title>Course Payment</title>
 </head>
 <body>
-	<form action="./charge.php" method="post" id="payment-form">
-		  <div class="form-row">
-		    <label for="card-element" class="form-control">
-		      Credit or debit card
-		    </label>
-		    <div id="card-element">
-		      <!-- A Stripe Element will be inserted here. -->
-		    </div>
+	<div class="container">	
+		<h2 class="my-4 text-center">Intro to React Course [$50]</h2>
+		<form action="./charge.php" method="post" id="payment-form">
+			  <div class="form-row">
+		    	<input type="text" name="first_name" class="form-control mb-3 StripeElement StripeElement--empty" placeholder="First Name">
+		    	<input type="text" name="last_name" class="form-control mb-3 StripeElement StripeElement--empty" placeholder="Last Name">
+		    	<input type="email" name="email" class="form-control mb-3 StripeElement StripeElement--empty" placeholder="Email Address">
 
-		    <!-- Used to display form errors. -->
-		    <div id="card-errors" role="alert"></div>
-		  </div>
+			    <div id="card-element" class="form-control">
+			      <!-- A Stripe Element will be inserted here. -->
+			    </div>
 
-		  <button>Submit Payment</button>
-	</form>
+			    <!-- Used to display form errors. -->
+			    <div id="card-errors" role="alert"></div>
+			  </div>
+
+			  <button>Submit Payment</button>
+		</form>
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://js.stripe.com/v3/"></script>	
 	<script src="./js/charge.js"></script>	
 </body>
